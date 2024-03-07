8b89b00a9244b0d17c2a8a2d53600919f67b9ee9
Ricardo Santoyo Priego <santoyoricardo838@gmail.com>
2024-03-07 08:24:02 -0600

    Version 1.1

---
+++ src/Admistrar/ControlConexion.java
@@ -0,0 +1,26 @@
+package Admistrar;
+
+public class ControlConexion {
+    private String Red;
+    private int Megas;
+    private String Compañia;
+
+    public int getMegas() {
+        return Megas;
+    }
+    public void setMegas(int megas) {
+        Megas = megas;
+    }
+    public String getCompañia() {
+        return Compañia;
+    }
+    public void setCompañia(String compañia) {
+        Compañia = compañia;
+    }
+    public String getRed() {
+        return Red;
+    }
+    public void setRed(String red) {
+        Red = red;
+    }
+}
